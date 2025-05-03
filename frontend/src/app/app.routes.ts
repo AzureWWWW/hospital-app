bf9d9836b9ec6ab8c71acab88616753fe2fdddbe
Thanh Pham <phamvuvanthanh2407@gmail.com>
2025-05-04 00:15:02 +0300

    fix security practices

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { authGuard } from './guard/auth.guard';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { NewAppointmentComponent } from './components/new-appointment/new-appointment.component';
 
 export const routes: Routes = [
   {
@@ -81,11 +80,6 @@ export const routes: Routes = [
         loadComponent: () =>{
           return import('./my-profile/my-profile.component').then((m)=> m.MyProfileComponent);
         }
-      },
-      {
-        path: 'new_appointment',
-        pathMatch: 'full',
-        component: NewAppointmentComponent
       },
       {
         path: 'admin_new_appointment',
